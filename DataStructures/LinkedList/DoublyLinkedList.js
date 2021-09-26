ccb22c7dbb3631d893829bdc6cd5dd1b10b0041a
Leouel <leouel.guanzon@gmail.com>
2021-09-25 22:51:57 -0700

    reorganized

---
+++ DataStructures/LinkedList/DoublyLinkedList.js
@@ -0,0 +1,24 @@
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.next = null;
+        this.prev = null;
+    }
+}
+
+class DoublyLinkedList{
+    constructor(){
+        this.head = null;
+        this.tail = null;
+        this.size = 0;
+    }
+
+    //insertAtBeginning = (value) => {}
+    //insertAtEnd = (value) => {}
+    //insertAtIndex = (value, index) => {}
+    //removeAtBeginning = (value) => {}
+    //removeAtEnd = (value) => {}
+    //removeAtIndex = (value, index) => {}
+    //printDLL = () => {}
+    
+}
